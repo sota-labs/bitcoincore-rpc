74d18a5155d2177123a26e7c486288ecf9020f60
Sotatek-MinhNguyen11 <minh.nguyen11@sotatek.com>
2024-04-11 17:20:09 +0700

    refactor code, v0.1.8

--- src/lib.rs
+++ src/lib.rs
@@ -16,10 +16,7 @@
 #![crate_name = "bitcoincore_rpc_sotatek"]
 #![crate_type = "rlib"]
 
-#[macro_use]
 extern crate log;
-// #[allow(unused)]
-#[macro_use] // `macro_use` is needed for v1.24.0 compilation.
 extern crate serde;
 
 pub extern crate jsonrpc;
@@ -27,25 +24,7 @@ pub extern crate jsonrpc;
 pub extern crate bitcoincore_rpc_json;
 pub use crate::json::bitcoin;
 pub use bitcoincore_rpc_json as json;
-use json::bitcoin::consensus::{Decodable, ReadExt};
-use json::bitcoin::hex::HexToBytesIter;
 
 mod client;
-mod error;
-mod queryable;
 
 pub use crate::client::*;
-pub use crate::error::Error;
-pub use crate::queryable::*;
-
-fn deserialize_hex<T: Decodable>(hex: &str) -> Result<T> {
-    let mut reader = HexToBytesIter::new(hex)?;
-    let object = Decodable::consensus_decode(&mut reader)?;
-    if reader.read_u8().is_ok() {
-        Err(Error::BitcoinSerialization(bitcoin::consensus::encode::Error::ParseFailed(
-            "data not consumed entirely when explicitly deserializing",
-        )))
-    } else {
-        Ok(object)
-    }
-}
